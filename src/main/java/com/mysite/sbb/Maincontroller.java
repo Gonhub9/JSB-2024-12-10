293d774870b5917a1ba4a42df38d09edcfd06008
Gonhub9 <ho_gok@naver.com>
2024-12-10 10:36:42 +0900

    Controller

---
+++ src/main/java/com/mysite/sbb/Maincontroller.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Maincontroller {
+    @GetMapping("/")
+    public void home() {
+        System.out.println("Home page requested");
+    }
+    @GetMapping("/about")
+    public void about() {
+        System.out.println("about");
+    }
+}
