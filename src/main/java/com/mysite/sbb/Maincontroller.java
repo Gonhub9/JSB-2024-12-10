0baeb8561e0904d833a43d805f478bdfb73bbd2d
Gonhub9 <ho_gok@naver.com>
2024-12-10 10:45:42 +0900

    no action method name is must be readable

--- src/main/java/com/mysite/sbb/Maincontroller.java
+++ src/main/java/com/mysite/sbb/Maincontroller.java
@@ -7,10 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Maincontroller {
     @GetMapping("/")
     public void home() {
-        System.out.println("Home page requested");
+        System.out.println("Home page");
+        this.contact();
     }
+
     @GetMapping("/about")
     public void about() {
         System.out.println("about");
+        this.contact();
+    }
+
+    public void contact() {
+        System.out.println("contact");
     }
 }
